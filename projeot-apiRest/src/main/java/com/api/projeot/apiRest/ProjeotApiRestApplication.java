25fb8bee1c3f6227dcfb609e7b000df1ecad1ccf
ruan <ruandasilvasantos49@gmail.com>
2025-08-27 11:32:35 -0300

    configuração do ambiente java

---
+++ projeot-apiRest/src/main/java/com/api/projeot/apiRest/ProjeotApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.api.projeot.apiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjeotApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjeotApiRestApplication.class, args);
+	}
+
+}
---
+++ projeot-apiRest/src/test/java/com/api/projeot/apiRest/ProjeotApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.projeot.apiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeotApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
